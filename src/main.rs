05815d1f367e0a32850acbe88995ccb362b9c994
Dmitri Chebotarev <dmitri.chebotarev@gmail.com>
2024-03-10 22:59:55 +0000

    Basic number lexing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
+#[derive(Debug)]
+enum TokenKind {
+    Plus,
+    Minus,
+    Asterisk,
+    Slash,
+    Equals,
+    ExclamationMark,
+    At,
+    SingleQuote,
+    DoubleQuote,
+    QuestionMark,
+    LessThan,
+    GreaterThan,
+    LeftBrace,
+    RightBrace,
+    LeftParan,
+    RightParan,
+    LeftBracket,
+    RightBracket,
+    Dot,
+}
+
 fn main() {
-    println!("Hello, world!");
+    for char in "+-*/=!@'\"?<>{}().".chars() {
+        let kind = match char {
+            '+' => TokenKind::Plus,
+            '-' => TokenKind::Minus,
+            '*' => TokenKind::Asterisk,
+            '/' => TokenKind::Slash,
+            '=' => TokenKind::Equals,
+            '!' => TokenKind::ExclamationMark,
+            '@' => TokenKind::At,
+            '\'' => TokenKind::SingleQuote,
+            '"' => TokenKind::DoubleQuote,
+            '?' => TokenKind::QuestionMark,
+            '<' => TokenKind::LessThan,
+            '>' => TokenKind::GreaterThan,
+            '{' => TokenKind::LeftBrace,
+            '}' => TokenKind::RightBrace,
+            '(' => TokenKind::LeftParan,
+            ')' => TokenKind::RightParan,
+            '[' => TokenKind::LeftBracket,
+            ']' => TokenKind::RightBracket,
+            '.' => TokenKind::Dot,
+            _ => panic!("unknown character: '{}'", char)
+        };
+
+        println!("{:?}", kind)
+    }
 }
