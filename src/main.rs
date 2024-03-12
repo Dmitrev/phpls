6372474acd27a4bd25ac741213c1c7dc1aef54b4
Dmitri Chebotarev <dmitri.chebotarev@gmail.com>
2024-03-12 23:43:01 +0000

    Moved existing code to lexer struct

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-#[derive(Debug)]
-enum TokenKind {
+#[derive(Debug, PartialEq)]
+enum Token {
     Plus,
     Minus,
     Asterisk,
@@ -19,33 +19,134 @@ enum TokenKind {
     LeftBracket,
     RightBracket,
     Dot,
+    Integer(i32),
+    Illegal,
+    EOF,
+}
+
+struct Lexer<'a> {
+    input: &'a str,
+    position: usize,
+}
+
+impl<'a> Lexer<'a> {
+    fn new(input: &'a str) -> Self {
+        Lexer { input, position: 0 }
+    }
+
+    fn next_char(&self) -> Option<char> {
+        self.input.chars().nth(self.position)
+    }
+
+    fn advance(&mut self) {
+        self.position += 1;
+    }
+
+    fn next_token(&mut self) -> Token {
+        let char = self.input.chars().nth(self.position);
+
+        if let Some(c) = char {
+            return match c {
+                '+' => {
+                    self.advance();
+                    Token::Plus
+                },
+                '-' => {
+                    self.advance();
+                    Token::Minus
+                },
+                '*' => {
+                    self.advance();
+                    Token::Asterisk
+                },
+                '/' => {
+                    self.advance();
+                    Token::Slash
+                },
+                '=' => {
+                    self.advance();
+                    Token::Equals
+                },
+                '!' => {
+                    self.advance();
+                    Token::ExclamationMark
+                },
+                '@' => {
+                    self.advance();
+                    Token::At
+                },
+                '\'' => {
+                    self.advance();
+                    Token::SingleQuote
+                },
+                '"' => {
+                    self.advance();
+                    Token::DoubleQuote
+                },
+                '?' => {
+                    self.advance();
+                    Token::QuestionMark
+                },
+                '<' => {
+                    self.advance();
+                    Token::LessThan
+                },
+                '>' => {
+                    self.advance();
+                    Token::GreaterThan
+                },
+                '{' => {
+                    self.advance();
+                    Token::LeftBrace
+                },
+                '}' => {
+                    self.advance();
+                    Token::RightBrace
+                },
+                '(' => {
+                    self.advance();
+                    Token::LeftParan
+                },
+                ')' => {
+                    self.advance();
+                    Token::RightParan
+                },
+                '[' => {
+                    self.advance();
+                    Token::LeftBracket
+                },
+                ']' => {
+                    self.advance();
+                    Token::RightBracket
+                },
+                '.' => {
+                    self.advance();
+                    Token::Dot
+                },
+                // // '0'..='9' => parse_num(chars_iter.clone()),
+                // _ => panic!("unknown character: '{}'", char)
+                _ => {
+                    self.advance();
+                    Token::Illegal
+                },
+            };
+        }
+        
+        Token::EOF
+    }
 }
 
 fn main() {
-    for char in "+-*/=!@'\"?<>{}().".chars() {
-        let kind = match char {
-            '+' => TokenKind::Plus,
-            '-' => TokenKind::Minus,
-            '*' => TokenKind::Asterisk,
-            '/' => TokenKind::Slash,
-            '=' => TokenKind::Equals,
-            '!' => TokenKind::ExclamationMark,
-            '@' => TokenKind::At,
-            '\'' => TokenKind::SingleQuote,
-            '"' => TokenKind::DoubleQuote,
-            '?' => TokenKind::QuestionMark,
-            '<' => TokenKind::LessThan,
-            '>' => TokenKind::GreaterThan,
-            '{' => TokenKind::LeftBrace,
-            '}' => TokenKind::RightBrace,
-            '(' => TokenKind::LeftParan,
-            ')' => TokenKind::RightParan,
-            '[' => TokenKind::LeftBracket,
-            ']' => TokenKind::RightBracket,
-            '.' => TokenKind::Dot,
-            _ => panic!("unknown character: '{}'", char)
-        };
+    let input = "+-*/=!@\"?<>{}()[].";
+    let mut lexer = Lexer::new(input);
 
-        println!("{:?}", kind)
+    loop {
+        let token = lexer.next_token();
+        println!("{:?}", token);
+
+        if token == Token::EOF {
+            break;
+        }
     }
 }
+
